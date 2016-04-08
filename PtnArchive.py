0c3a3f59083a07af6ac9c1f0a63eed1f956174a4
Itay Wiseman <itayw@traiana.com>
2016-04-08 16:37:20 -0400

    Connections utility

--- PtnArchive.py
+++ PtnArchive.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, json
-import cx_Oracle
+from flask import Flask, render_template, request
 
+from resources import connections
 
 app = Flask(__name__)
 
@@ -10,14 +10,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
 @app.route('/showToolForm')
 def showToolForm():
-    return render_template("toolForm.html")
+
+    # ---------Tool initial Settings ------
+    connections.loadSettings()
+    # -------------------------------------
+
+    return render_template("toolForm.html", owners=getAllOwners())
+
 
 @app.route('/startTool', methods=['POST'])
 def startTool():
     _query = request.form['selectQuery']
-    print _query
+
+
+def getAllOwners():
+    return ["HR4_PROD", "HR5_UAT", "TRM_JPMC"]  # get all owners list
+
 
 if __name__ == '__main__':
     app.run()
